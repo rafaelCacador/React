90a5be85d29c0969c903d91af9b638df61f7646f
Rafael Cacador <rafael.dioscanio@viannasempre.com.br>
2024-04-26 16:21:15 -0300

    Colocando CSS

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
+import Frase from "./components/Frase";
 
 function App() {
   return (
@@ -9,11 +10,15 @@ function App() {
     //logo todos os elementos jsx devem estar dentro de um elemento (div por exemplo)
     // class do html é o className do React 
     <div className="App">
-      <Pessoa nome = "Rafael" idade = "19" profissao = "Analista de TI" foto="..\public\logo192.png"/>
+      <h1>Testando CSS</h1>
+      <Pessoa nome = "Rafael" idade = "19" profissao = "Analista de TI" foto="eu.jpeg"/>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Rafael" />
       <SayMyName nome="Otávio" />
       <SayMyName nome="Ricardo" />
       <HelloWorld />
+      
       
     </div>
   );
--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css'
+
 function Frase(){
     return(
-        <div>
-            <p>Esse é o componente de uma frase</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Esse é o componente de uma frase</p>
         </div>
 
     )
